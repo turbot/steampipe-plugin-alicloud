184f273881697c51a66b8045f236f4873b0e8865
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-02-19 19:57:19 +0530

    Add alicloud_ecs_disk table. Closes #3

--- alicloud/utils.go
+++ alicloud/utils.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ram"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/vpc"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
@@ -103,6 +104,14 @@ func getEnv(ctx context.Context) (region string, ak string, secret string, err e
 	}
 
 	return region, ak, secret, nil
+}
+
+func connectEcs(ctx context.Context) (*ecs.Client, error) {
+	region, ak, secret, err := getEnv(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return ecs.NewClientWithAccessKey(region, ak, secret)
 }
 
 func connectRam(ctx context.Context) (*ram.Client, error) {
