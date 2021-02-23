d015a1ae48fa9c1ec0ec72641ffb5d2673ec6c70
rajeshbal65 <rajesh.bal@turbot.com>
2021-02-23 14:49:15 +0530

    Add alicloud_ecs_eni table

---
+++ alicloud/table_alicloud_ecs_eni.go
@@ -0,0 +1,263 @@
+package alicloud
+
+import (
+	"context"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/errors"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableAlicloudEcsEni(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "alicloud_ecs_eni",
+		Description: "Elastic Compute Service Eni",
+		List: &plugin.ListConfig{
+			Hydrate: listEcsEni,
+		},
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("id"),
+			Hydrate:    getEcsEni,
+		},
+		Columns: []*plugin.Column{
+			// Top columns
+			{
+				Name:        "name",
+				Description: "The name of the ENI.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("NetworkInterfaceName"),
+			},
+			{
+				Name:        "id",
+				Description: "An unique identifier for the ENI.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("NetworkInterfaceId"),
+			},
+			{
+				Name:        "type",
+				Description: "The type of the ENI. Valid values: 'Primary' and 'Secondary'",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "status",
+				Description: "The status of the ENI.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "vpc_id",
+				Description: "The ID of the VPC to which the ENI belongs.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "vswitch_id",
+				Description: "The ID of the VSwitch to which the ENI is connected.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("VSwitchId"),
+			},
+			{
+				Name:        "description",
+				Description: "The description of the ENI.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "creation_time",
+				Description: "The time when the ENI was created.",
+				Type:        proto.ColumnType_TIMESTAMP,
+			},
+			{
+				Name:        "zone_id",
+				Description: "The zone ID of the ENI.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "private_ip_address",
+				Description: "The private IP address of the ENI.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "mac_address",
+				Description: "The MAC address of the ENI.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "instance_id",
+				Description: "The ID of the instance to which the ENI is bound.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "service_id",
+				Description: "The ID of the distributor to which the ENI belongs.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServiceID"),
+			},
+			{
+				Name:        "service_managed",
+				Description: "Indicates whether the user is an Alibaba Cloud service or a distributor.",
+				Type:        proto.ColumnType_BOOL,
+			},
+			{
+				Name:        "queue_number",
+				Description: "The number of queues supported by the ENI.",
+				Type:        proto.ColumnType_INT,
+			},
+			{
+				Name:        "resource_group_id",
+				Description: "The ID of the resource group to which the ENI belongs.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "owner_id",
+				Description: "The ID of the account that owns the ENI.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "security_groupIds",
+				Description: "The IDs of the security groups to which the ENI belongs.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "associated_public_ip",
+				Description: "The public IP address associated with the secondary private IP address of the ENI.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "attachment",
+				Description: "Attachments of the ENI",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "private_ip_sets",
+				Description: "The private IP addresses of the ENI.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "ipv6_sets",
+				Description: "The IPv6 addresses assigned to the ENI.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("Ipv6Sets"),
+			},
+			{
+				Name:        "tags_src",
+				Description: "A list of tags attached with the resource.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("Tags.Tag"),
+			},
+
+			// steampipe standard columns
+			// {
+			// 	Name:        "tags",
+			// 	Description: resourceInterfaceDescription("tags"),
+			// 	Type:        proto.ColumnType_JSON,
+			// 	Transform:   transform.From(ecsEniTags),
+			// },
+			{
+				Name:        "title",
+				Description: resourceInterfaceDescription("title"),
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("NetworkInterfaceName"),
+			},
+
+			// alibaba standard columns
+			// {
+			// 	Name:        "account_id",
+			// 	Description: "The alicloud Account ID in which the resource is located.",
+			// 	Type:        proto.ColumnType_STRING,
+			// 	Hydrate:     getCommonColumns,
+			// 	Transform:   transform.FromField("AccountID"),
+			// },
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listEcsEni(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// Create service connection
+	client, err := connectEcs(ctx)
+	if err != nil {
+		plugin.Logger(ctx).Error("alicloud_ecs_eni.listEcsEni", "connection_error", err)
+		return nil, err
+	}
+	request := ecs.CreateDescribeNetworkInterfacesRequest()
+	request.Scheme = "https"
+	request.PageSize = requests.NewInteger(50)
+	request.PageNumber = requests.NewInteger(1)
+
+	count := 0
+	for {
+		response, err := client.DescribeNetworkInterfaces(request)
+		if err != nil {
+			plugin.Logger(ctx).Error("alicloud_ecs_eni.listEcsEni", "query_error", err, "request", request)
+			return nil, err
+		}
+		for _, eni := range response.NetworkInterfaceSets.NetworkInterfaceSet {
+			plugin.Logger(ctx).Warn("listEcsEni", "item", eni)
+			d.StreamListItem(ctx, eni)
+			count++
+		}
+		if count >= response.TotalCount {
+			break
+		}
+		request.PageNumber = requests.NewInteger(response.PageNumber + 1)
+	}
+	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+
+func getEcsEni(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getEcsEni")
+
+	// Create service connection
+	client, err := connectEcs(ctx)
+	if err != nil {
+		plugin.Logger(ctx).Error("alicloud_ecs_eni.getEcsEni", "connection_error", err)
+		return nil, err
+	}
+
+	var id string
+	if h.Item != nil {
+		NetworkInterfaceSet := h.Item.(ecs.NetworkInterfaceSet)
+		id = NetworkInterfaceSet.NetworkInterfaceId
+	} else {
+		id = d.KeyColumnQuals["id"].GetStringValue()
+	}
+
+	request := ecs.CreateDescribeNetworkInterfaceAttributeRequest()
+	request.Scheme = "https"
+	request.NetworkInterfaceId = id
+
+	response, err := client.DescribeNetworkInterfaceAttribute(request)
+	if serverErr, ok := err.(*errors.ServerError); ok {
+		plugin.Logger(ctx).Error("alicloud_ecs_eni.getEcsEni", "query_error", serverErr, "request", request)
+		return nil, serverErr
+	}
+
+
+	return response, nil
+}
+
+//// TRANSFORM FUNCTIONS
+
+func ecsEniTags(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	Eni := d.HydrateItem.(ecs.NetworkInterfaceSet)
+
+	var turbotTagsMap map[string]string
+
+	if Eni.Tags.Tag == nil {
+		return nil, nil
+	}
+
+	turbotTagsMap = map[string]string{}
+	for _, i := range Eni.Tags.Tag {
+		turbotTagsMap[i.TagKey] = i.TagValue
+	}
+
+	return turbotTagsMap, nil
+}
