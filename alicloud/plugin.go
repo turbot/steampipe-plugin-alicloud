5b5eb9e37bb7bcd9d7e747152b9e4f424eb5addc
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2021-03-03 23:33:53 +0530

    add alicloud_vpc_vpn_ssl_server table. Closes #16 (#19)
    Co-authored-by: Subhajit Kumar Mondal <subhajit@turbot.com>
    Co-authored-by: rajeshbal65 <rajesh.bal@turbot.com>

--- alicloud/plugin.go
+++ alicloud/plugin.go
@@ -37,6 +37,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"alicloud_vpc_vpn_connection":       tableAlicloudVpcVpnConnection(ctx),
 			"alicloud_vpc_vpn_customer_gateway": tableAlicloudVpcVpnCustomerGateway(ctx),
 			"alicloud_vpc_vpn_gateway":          tableAlicloudVpcVpnGateway(ctx),
+			"alicloud_vpc_vpn_ssl_server":       tableAlicloudVpcVpnSslServer(ctx),
 			"alicloud_vpc_vswitch":              tableAlicloudVpcVSwitch(ctx),
 		},
 	}
