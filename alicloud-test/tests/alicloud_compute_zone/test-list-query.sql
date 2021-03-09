b63aee13e253448054a93ff448bcd92cc552fdbf
bigdatasourav <bigdatasourav@gmail.com>
2021-03-09 15:27:10 +0530

    test update

--- alicloud-test/tests/alicloud_compute_zone/test-list-query.sql
+++ alicloud-test/tests/alicloud_compute_zone/test-list-query.sql
@@ -1,3 +1,3 @@
 select zone_id, local_name, available_instance_types
 from alicloud_compute_zone
-where akas::text = '["{{output.resource_aka.value}}"]';
+where zone_id = '{{ output.zone_id.value }}';
