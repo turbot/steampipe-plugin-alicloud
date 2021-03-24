e2523a3cef2c0574befe08c7488dc94adbdf2028
bigdatasourav <bigdatasourav@gmail.com>
2021-03-24 16:44:52 +0530

    test update

--- alicloud-test/tests/alicloud_elastic_container/variables.tf
+++ alicloud-test/tests/alicloud_elastic_container/variables.tf
@@ -23,45 +23,31 @@ data "null_data_source" "resource" {
   }
 }
 
+
+
 // If there is not specifying vpc_id, the module will launch a new vpc
 resource "alicloud_vpc" "named_test_resource" {
-  count      = var.vpc_id == "" ? 1 : 0
-  cidr_block = var.vpc_cidr
+  vpc_name   = var.resource_name
+  cidr_block = "172.16.0.0/12"
 }
 
 // According to the vswitch cidr blocks to launch several vswitches
 resource "alicloud_vswitch" "named_test_resource" {
-  count             = length(var.vswitch_ids) > 0 ? 0 : length(var.vswitch_cidrs)
-  vpc_id            = var.vpc_id == "" ? join("", alicloud_vpc.named_test_resource.*.id) : var.vpc_id
-  cidr_block        = element(var.vswitch_cidrs, count.index)
-  availability_zone = element(var.availability_zone, count.index)
+  vpc_id     = alicloud_vpc.named_test_resource.id
+  cidr_block = "172.16.0.0/21"
+  zone_id    = "us-east-1b"
 }
 
 resource "alicloud_cs_kubernetes" "named_test_resource" {
-  count                 = 1
-  master_vswitch_ids    = length(var.vswitch_ids) > 0 ? split(",", join(",", var.vswitch_ids)) : length(var.vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.named_test_resource.*.id))
-  worker_vswitch_ids    = length(var.vswitch_ids) > 0 ? split(",", join(",", var.vswitch_ids)) : length(var.vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.named_test_resource.*.id))
-  master_instance_types = var.master_instance_types
-  worker_instance_types = var.worker_instance_types
-  worker_number         = var.worker_number
-  node_cidr_mask        = var.node_cidr_mask
-  enable_ssh            = var.enable_ssh
-  install_cloud_monitor = var.install_cloud_monitor
-  cpu_policy            = var.cpu_policy
-  proxy_mode            = var.proxy_mode
-  password              = var.password
-  pod_cidr              = var.pod_cidr
-  service_cidr          = var.service_cidr
+  master_vswitch_ids    = [alicloud_vswitch.named_test_resource.id, alicloud_vswitch.named_test_resource.id, alicloud_vswitch.named_test_resource.id]
+  worker_vswitch_ids    = [alicloud_vswitch.named_test_resource.id, alicloud_vswitch.named_test_resource.id, alicloud_vswitch.named_test_resource.id]
+  master_instance_types = ["ecs.c5.large", "ecs.c5.large", "ecs.c5.large"]
+  worker_instance_types = ["ecs.c5.large", "ecs.c5.large", "ecs.c5.large"]
+  worker_number         = "3"
+  pod_cidr              = "10.10.0.0/24"
+  service_cidr          = "192.168.0.0/24"
+  password              = "Test1234!"
   name                  = var.resource_name
-
-  dynamic "addons" {
-    for_each = var.cluster_addons
-    content {
-      name     = lookup(addons.value, "name", var.cluster_addons)
-      config   = lookup(addons.value, "config", var.cluster_addons)
-      disabled = lookup(addons.value, "disabled", var.cluster_addons)
-    }
-  }
 }
 
 
