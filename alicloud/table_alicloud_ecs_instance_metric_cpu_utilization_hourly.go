b3d5543d2a1c240b6d2fb24e441a5d907f2c1c92
ParthaI <partha@turbot.com>
2022-03-24 18:32:24 +0530

    Add table alicloud_ecs_instance_metric_cpu_utilization_hourly closes #243

---
+++ alicloud/table_alicloud_ecs_instance_metric_cpu_utilization_hourly.go
@@ -0,0 +1,38 @@
+package alicloud
+
+import (
+	"context"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableAlicloudEcsInstanceMetricCpuUtilizationHourly(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "alicloud_ecs_instance_metric_cpu_utilization_hourly",
+		Description: "Alicloud ECS Instance Cloud Monitor Metrics - CPU Utilization (Hourly)",
+		List: &plugin.ListConfig{
+			ParentHydrate: listEcsInstance,
+			Hydrate:       listEcsInstanceMetricCpuUtilizationHourly,
+		},
+		GetMatrixItem: BuildRegionList,
+		Columns: cmMetricColumns(
+			[]*plugin.Column{
+				{
+					Name:        "instance_id",
+					Description: "The ID of the instance.",
+					Type:        proto.ColumnType_STRING,
+					Transform:   transform.FromField("DimensionValue"),
+				},
+			}),
+	}
+}
+
+func listEcsInstanceMetricCpuUtilizationHourly(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	data := h.Item.(ecs.Instance)
+	return listCMMetricStatistics(ctx, d, "HOURLY", "acs_ecs_dashboard", "CPUUtilization", "instanceId", data.InstanceId)
+}
