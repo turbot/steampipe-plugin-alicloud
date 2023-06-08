aa29cbe34e6f3b9943e7c5bf2f288e32cd254410
Karan Popat <karan@turbot.com>
2023-06-08 16:10:14 +0530

    Update alicloud/errors.go
    Co-authored-by: ParthaI <47887552+ParthaI@users.noreply.github.com>

--- alicloud/errors.go
+++ alicloud/errors.go
@@ -7,7 +7,7 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
-// isNotFoundError:: function which returns an ErrorPredicate for Alicloud API calls
+// isNotFoundError:: function which returns an ErrorPredicateWithContext for Alicloud API calls
 func isNotFoundError(notFoundErrors []string) plugin.ErrorPredicateWithContext {
 	return func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData, err error) bool {
 		alicloudConfig := GetConfig(d.Connection)
